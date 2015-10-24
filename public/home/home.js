5de151de512370c40d12fece47c1aefc9e92193d
Jack <jrnichols@umass.edu>
2015-10-24 16:25:57 -0400

    html changes

--- public/home/home.js
+++ public/home/home.js
@@ -3,7 +3,8 @@ angular.module('app')
     var home = this;
     home.categories = [{name: "Books", query: ""},{name: "Event Tickets", query: ""}, {name: "Furniture", query: ""},
     {name: "Clothing", query: ""}, {name: "Lost and Found", query: ""}, {name: "Rides", query: ""}, {name: "Electronics", query: ""},
-    {name: "Dorm Supplies", query: ""}, {name: "Automobiles", query: ""}, {name: "Housing", query: ""}, {name: "Misc", query: ""}];
+    {name: "Dorm Supplies", query: ""}, {name: "Automobiles", query: ""}, {name: "Housing", query: ""}, {name: "Misc", query: ""}
+    ,{name: "Jobs", query: ""}];
 
     home.openCategory = function(category){
       console.log(category);
