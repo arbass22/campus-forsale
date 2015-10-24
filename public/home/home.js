744c456f50981f6fc445d790705df0f80e6f5ece
Jack <jrnichols@umass.edu>
2015-10-24 05:19:29 -0400

    Playing with John Cena

--- public/home/home.js
+++ public/home/home.js
@@ -1,6 +1,12 @@
 angular.module('app')
   .controller('homeController', function() {
     var home = this;
-  
+    home.categories = [{name: "Books", query: ""},{name: "Event Tickets", query: ""}, {name: "Furniture", query: ""},
+    {name: "Clothing", query: ""}, {name: "Lost and Found", query: ""}, {name: "Rides", query: ""}, {name: "Electronics", query: ""},
+    {name: "Dorm Supplies", query: ""}, {name: "Automobiles", query: ""}, {name: "Housing", query: ""}, {name: "Misc", query: ""}];
+
+    home.openCategory = function(category){
+
+    };
 
   });
