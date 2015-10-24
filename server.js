b6fa55c3ff45571900c3e48aaf68ee3614cf2bd9
arbass22 <arbass@umass.edu>
2015-10-24 01:14:33 -0400

    Added routing to server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
+var ItemHandler = require('./routes/ItemHandler')
+
 app.use(express.static('public'));
+
+app.use('/api/items', ItemHandler)
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
