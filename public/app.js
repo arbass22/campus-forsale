ac26c0cba47c1930f059cb857ca492a479583b41
Justin Kennedy <jekennedy@umass.edu>
2015-10-24 21:21:45 -0400

    huge restructure

---
+++ public/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('app', ['ngRoute'])
+  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    $routeProvider
+      .when('/', {
+        templateUrl: 'home.html'
+      })
+      .when('/category', {
+        templateUrl: 'category.html'
+      })
+      .when('/item', {
+        templateUrl: 'item.html'
+      })
+      .when('/create', {
+        templateUrl: 'create.html'
+      });
+  }]);
--- public/item/item.js
+++
@@ -1,5 +0,0 @@
-angular.module('app')
-  .controller('itemController', function() {
-    var item = this;
-
-  });
